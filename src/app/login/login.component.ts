0c20c6e76da2a734a0f3e58e7e64e19ea1ba638a
kirill-bobr <kirillbobryakov82@gmail.com>
2020-08-27 01:47:14 +0800

    Modify Login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss', '../app.component.scss']
 })
 export class LoginComponent {}
