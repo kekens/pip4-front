35978c4ab467a0c98bd7a93ab12347de2a5658f9
kirill-bobr <kirillbobryakov82@gmail.com>
2020-08-27 01:48:50 +0800

    Modify App-root component
    Added Header and Home components in HTML and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
