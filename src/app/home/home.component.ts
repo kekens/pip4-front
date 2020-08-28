306b7af6c63bae97f63b783ddf1c98302ce87dcd
kirill-bobr <kirillbobryakov82@gmail.com>
2020-08-28 17:13:35 +0800

    Add interactive clock on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', '../app.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+  time = new Date();
+  timer;
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
+
+}
+
+
